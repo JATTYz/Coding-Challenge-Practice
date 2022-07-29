99b9002535352e5e7019ecba92fcd2456b739931
Jaturon Tepjuk <jaturon.tepjuk@gmail.com>
2022-07-30 01:00:51 +1000

    114. Linked list cycle

---
+++ 141LinkedListCycle.java
@@ -0,0 +1,24 @@
+class solution {
+
+public static void main(String[] args) {
+    
+}
+
+public static boolean hasCycle(ListNode head){
+
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast){
+            return true;
+        }
+    }
+
+    return false;
+
+}
+}
