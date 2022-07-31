70d12dca96fff613a36a2ab66cf7ecd68b538aae
Jaturon Tepjuk <jaturon.tepjuk@gmail.com>
2022-08-01 01:23:00 +1000

    14.LongestComminPrefix

--- 141LinkedListCycle.java
+++ 141LinkedListCycle.java
@@ -1,24 +1,24 @@
-class solution {
+// class solution {
 
-public static void main(String[] args) {
+// public static void main(String[] args) {
     
-}
+// }
 
-public static boolean hasCycle(ListNode head){
+// public static boolean hasCycle(ListNode head){
 
-    ListNode fast = head;
-    ListNode slow = head;
+//     ListNode fast = head;
+//     ListNode slow = head;
 
-    while(fast != null && fast.next != null){
-        slow = slow.next;
-        fast = fast.next.next;
+//     while(fast != null && fast.next != null){
+//         slow = slow.next;
+//         fast = fast.next.next;
 
-        if(slow == fast){
-            return true;
-        }
-    }
+//         if(slow == fast){
+//             return true;
+//         }
+//     }
 
-    return false;
+//     return false;
 
-}
-}
+// }
+// }
