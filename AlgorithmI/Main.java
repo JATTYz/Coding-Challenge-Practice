ca2b3accfc19f73771f4391bcfa6826346885331
Jaturon Tepjuk <jaturon.tepjuk@gmail.com>
2022-08-14 00:58:32 +1000

    b

--- AlgorithmI/Main.java
+++ AlgorithmI/Main.java
@@ -3,15 +3,35 @@ package AlgorithmI;
 import java.util.Arrays;
 
 import javax.naming.NameAlreadyBoundException;
+import javax.sound.sampled.SourceDataLine;
 
 public class Main {
    public static void main(String[] args) {
-     int[] nums = {0,1,0,3,12};
-      
-    MoveZone test = new MoveZone();
-    test.moveZeros(nums); 
-      System.out.println(Arrays.toString(nums));
+     int[] nums = {0,1,2,3,12};
+     
+     TwoSum test = new TwoSum();
+     int[] answer = test.twoSum(nums, 4);
+    
+      // int[] answer = twoSum(nums, 12);
+      // twoSum(nums, 4);
+      System.out.println(Arrays.toString(answer));
 
    } 
+
+   public static int[] twoSum(int[] numbers, int target){
+        
+      int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+  }
 
 }
