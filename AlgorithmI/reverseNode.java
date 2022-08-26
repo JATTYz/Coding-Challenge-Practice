c4e2c594595611861e3e203d4e7080b2514765ce
Jattyz <jaturon.tepjuk@gmail.com>
2022-08-27 01:45:37 +1000

    reverse node

---
+++ AlgorithmI/reverseNode.java
@@ -0,0 +1,18 @@
+package AlgorithmI;
+
+public class reverseNode {
+    // ListNode curr = head;
+    // ListNode next = null;
+    // ListNode previousNode = null;
+    
+    
+    // while(curr != null){
+    //    next = curr.next;
+    //    curr.next = previousNode;
+    //     previousNode = curr;
+    //     curr = next;
+    
+    // }
+    // return previousNode;
+
+}
