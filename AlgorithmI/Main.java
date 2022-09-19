ce0eb9b8c40a3c30db2c1096a5db78e4ac263273
Jattyz <jaturon.tepjuk@gmail.com>
2022-09-20 00:23:08 +1000

    test hashSet

--- AlgorithmI/Main.java
+++ AlgorithmI/Main.java
@@ -1,17 +1,33 @@
 package AlgorithmI;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 import AlgorithmI.SlidingWindow.Permutation;
 
 
 public class Main {
    public static void main(String[] args) {
-    WordPattern test = new WordPattern();
+
+      HashMap<Integer, Integer> arr = new HashMap<>();
+      HashSet<Integer> a = new HashSet<>();
+      Set<Integer> b = new TreeSet<>();
 
 
-    test.test("abba", "dog cat cat dog");
-    // test.solution("abba", "dog cat cat dog"); 
+      int[] test = {1,1,12,3,4,5,4,4,6};
+
+      for(int i =0; i< test.length; i++){
+         arr.put(test[i], arr.getOrDefault(test[i],0)+1);
+         a.add(test[i]);
+         b.add(test[i]);
+      }
+
+      System.out.println(arr);
+      System.out.println(b);
+
    }
 
 }
