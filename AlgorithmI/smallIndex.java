d291153c0c0ba24dffcb792a1435774dfb5ea81d
Jattyz <jaturon.tepjuk@gmail.com>
2022-09-23 22:14:27 +1000

    smallIndex

---
+++ AlgorithmI/smallIndex.java
@@ -0,0 +1,18 @@
+package AlgorithmI;
+
+public class smallIndex {
+    
+    public int solution(int[] nums){
+
+        for(int i = 0 ; i< nums.length; i++){
+
+            int x = i % 10;
+
+            if(x == nums[i]){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
