70e24e387afb4b0c2cf56778170fc4f8780370cd
Jattyz <jaturon.tepjuk@gmail.com>
2022-09-26 01:19:39 +1000

    bb sort

--- AlgorithmI/StringMatching.java
+++ AlgorithmI/StringMatching.java
@@ -21,4 +21,21 @@ public class StringMatching {
         }
         return l;
     }
+
+    public static int[] bubbleSort(int[] arr){
+
+        for(int i = 0; i< arr.length; i++){
+
+            for(int j = 0;j < arr.length - i -1; j++){
+
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }        
+        return arr;
+    }
+
 }
