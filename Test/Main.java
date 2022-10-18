cca4c96778776b5380fdf5f623bc5644b1738255
Jattyz <jaturon.tepjuk@gmail.com>
2022-10-18 23:04:55 +1100

    d

--- Test/Main.java
+++ Test/Main.java
@@ -5,8 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         
-        String a =  "Mismatch slot type! Staff must park in staff slots or a visitor must park on visitor slots";
-        System.out.println(a.startsWith("This"));
     }
 
 }
