2dc851c0e01e70992fd4bf2b92d83ef72e7a3d99
Jattyz <jaturon.tepjuk@gmail.com>
2022-10-20 22:14:06 +1100

    hasCode

--- Test/Main.java
+++ Test/Main.java
@@ -4,7 +4,14 @@ package Test;
 public class Main {
 
     public static void main(String[] args) {
-        
+		    String s = "GGGKDJLSFJKLSFJSDKLF"; 
+            s = "GGGKDJLSFJKLSFJSDKLF";
+
+
+            
+            System.out.println(s.hashCode());
+            
+            System.out.println(s.concat("DF"));
     }
 
 }
